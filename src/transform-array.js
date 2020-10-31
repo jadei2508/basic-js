dfeb11e4d11747e9124e762faca015fcda9839ee
romanalikevich <roma.alikevich@mail.ru>
2020-10-31 12:01:55 +0300

    fix: update transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    // throw new CustomError('Not implemented');
+    if (arr === undefined) {
+        return 'Unable to determine the time of year!'
+    }
+    if (arr.length === 0) return [];
+    // if (Object.entries(arr).length > 0) throw new Error();
+    // let index = 0;
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            if (i < arr.length - 1) {
+                i++;
+            }
+        } else if (arr[i] === '--discard-prev') {
+            if (i - 1 >= 0 && arr[i - 2] !== '--discard-next') {
+                result.pop();
+            }
+        } else if (arr[i] === '--double-next') {
+            if (i < arr.length - 1) {
+                result.push(arr[i + 1])
+            }
+        } else if (arr[i] === '--double-prev') {
+            if (i >= 1 && arr[i - 2] !== '--discard-next') {
+                result.push(arr[i - 1]);
+            }
+        } else {
+            result.push(arr[i])
+        }
+    }
+    return result;
 };
+
