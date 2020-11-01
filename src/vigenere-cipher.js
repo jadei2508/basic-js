055eb20c50b17e7df3a02e1946ea4f220d1c0f9f
Roman <68894436+jadei2508@users.noreply.github.com>
2020-11-01 16:37:50 +0300

    feat: add encrypt and decrypt function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,39 @@
 const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(data = true) {
+    this.data = data;
+    this.words = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  }
+
+  encrypt(msg, key) {
+    const message = [...msg.toUpperCase()];
+    const keys = [...key.toUpperCase()];
+    let position = 0;
+    let result = message.map(element => {
+      if (this.words.includes(element)) {
+        let index = (this.words.indexOf(keys[position]) + this.words.indexOf(element)) % this.words.length;
+        position = ++position % keys.length;
+        return this.words[index];
+      }
+      return element;
+    } );
+    return this.data ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(msg, key) {
+    const message = [...msg.toUpperCase()];
+    const keys = [...key.toUpperCase()];
+    let position = 0;
+    let result = message.map(element => {
+      if (this.words.includes(element)) {
+        let index = this.words.indexOf(element) - this.words.indexOf(keys[position]);
+        if (index < 0) index += this.words.length;
+        position = ++position % keys.length;
+        return this.words[index];
+      }
+      return element;
+    } );
+    return this.data ? result.join('') : result.reverse().join('');
   }
 }
 
